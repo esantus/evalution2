fbf1925d9273797ecdcda5da9fdee20b7684ebd9
Enrico Santus <enrico_santus@Enricos-MacBook-Air.local>
2017-11-15 15:57:27 +0800

    Restructured Directories

---
+++ src/Baseline/Baseline.py
@@ -0,0 +1,5 @@
+"""
+This module includes a class for the creation of DSMs (either window- or dep-based) and a class
+with several methods for the use of vectors extracted from these DSMs in machine learning, for
+the identification of semantic relations.
+"""
