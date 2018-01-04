76c8fe5e2e0dcaa0d972bd52786259a8032a213f
liacoponi <jacoponi@gmail.com>
2018-01-04 16:47:34 -0500

    update URL

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
               'evalution.composes.semantic_space', 'evalution.composes.transformation',
               'evalution.composes.transformation.scaling', 'evalution.composes.transformation.dim_reduction',
               'evalution.composes.transformation.feature_selection'],
-    url='https://github.com/esantus/EVALution-2.0',
+    url='https://github.com/esantus/evalution2',
     license='MIT',
     author='Luca Iacoponi, Enrico Santus',
     author_email='jacoponi@gmail.com, esantus@gmail.com',
